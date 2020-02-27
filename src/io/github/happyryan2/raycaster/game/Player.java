851f3833853f27ab09d3a20f822bf9a137cd8669
HappyRyan2 <happyryan2@hotmail.com>
2020-02-26 19:30:30 -0800

    fixed bug
    bug: player starts off facing different directions, depending on where the user's mouse is on program start

--- src/io/github/happyryan2/raycaster/game/Player.java
+++ src/io/github/happyryan2/raycaster/game/Player.java
@@ -15,21 +15,26 @@ public class Player {
 	public static float screenSize = 400;
 	public static float res = fov / screenSize;
 	public static float renderDistance = 500;
+	public static boolean initialized = false;
 	public static void input() {
-		// if(true) { return; }
 		/* Get mouse inputs + adjust vision */
 		if(Screen.frame.isFocused()) {
 			Point screenPos = Screen.frame.getLocationOnScreen();
-			viewX += (MousePos.x - 397 - screenPos.x) / 2;
-			viewY -= (MousePos.y - 374 - screenPos.y) / 2;
-			// System.out.println("viewY: " + viewY);
-			if(viewY > 90) {
-				viewY = 90;
+			if(!initialized) {
+				Screen.robot.mouseMove(400, 400);
+				initialized = true;
 			}
-			else if(viewY < -90) {
-				viewY = -90;
+			else {
+				viewX += (MousePos.x - 397 - screenPos.x) / 2;
+				viewY -= (MousePos.y - 374 - screenPos.y) / 2;
+				if(viewY > 90) {
+					viewY = 90;
+				}
+				else if(viewY < -90) {
+					viewY = -90;
+				}
+				Screen.robot.mouseMove(400, 400);
 			}
-			Screen.robot.mouseMove(400, 400);
 		}
 		/* Get key inputs + move */
 		if(KeyInputs.keyW) {
